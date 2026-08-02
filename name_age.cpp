928d0bcee186c4038ba8504ce5e130868ebac1cc
Hazique Ali <haziqueali6694@gmail.com>
2026-08-03 04:03:24 +0500

    Add files via upload
    consolidated practice programs from previous repositories

---
+++ name_age.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+string name;
+int age;
+
+cout << "enter your name:" <<endl ;
+
+cin >> name ;
+cout<<"enter your age: " << endl;
+cin>> age ;
+cout << "Hello," << name <<"! you are" << age << "years old."<< endl;
+return 0 ;
+}
